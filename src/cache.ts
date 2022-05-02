95589bd6bf03280d3db9918807705559f6118c60
Chris Gavin <chris@chrisgavin.me>
2022-05-02 17:14:36 +0100

    Add some dependency caching.

---
+++ src/cache.ts
@@ -0,0 +1,39 @@
+import * as cache from "@actions/cache";
+import * as crypto from "crypto";
+import * as fs from "fs";
+import * as os from "os";
+import * as core from "@actions/core";
+
+const packageLockPath = "./package-lock.json";
+const cachePaths = [
+	"~/.cache/pip/",
+	"~/.cache/pypoetry/",
+];
+
+async function cacheKey(pythonVersion:string) {
+	const hash = crypto.createHash("sha256");	
+	hash.update(await fs.promises.readFile(packageLockPath));
+	return `python-cache-${os.type()}-${hash.digest("hex")}-${pythonVersion}`;
+}
+
+export async function restoreCache(pythonVersion:string):Promise<void> {
+	const key = await cacheKey(pythonVersion);
+	core.info(`Restoring cache with key ${key}.`);
+	await cache.restoreCache(cachePaths, key);
+}
+
+export async function saveCache(pythonVersion:string):Promise<void> {
+	const key = await cacheKey(pythonVersion);
+	core.info(`Saving cache with key ${key}.`);
+	try {
+		await cache.saveCache(cachePaths, key);
+	}
+	catch (e) {
+		if (e instanceof cache.ReserveCacheError) {
+			core.warning(e);
+		}
+		else {
+			throw e;
+		}
+	}
+}
--- src/post.ts
+++ src/post.ts
@@ -1,8 +1,11 @@
+import * as cache from "./cache";
 import * as core from "@actions/core";
+import * as pythonVersion from "./pythonVersion";
 import sourceMapSupport from "source-map-support";
 
 async function main() {
 	sourceMapSupport.install();
+	await cache.saveCache(await pythonVersion.getPythonVersion());
 }
 
 main().catch(error => core.setFailed(error.stack || error));
