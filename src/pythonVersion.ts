db909390a5615ef984f4e1392feca44b678c35f8
Chris Gavin <chris@chrisgavin.me>
2024-09-02 10:06:43 +0100

    Update linting configuration.

--- src/pythonVersion.ts
+++ src/pythonVersion.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import * as inputs from "./inputs";
+import * as inputs from "./inputs.js";
 
 export async function getPythonVersion():Promise<string> {
 	const contents = await fs.promises.readFile(inputs.get()["python-version-path"], "utf8");
